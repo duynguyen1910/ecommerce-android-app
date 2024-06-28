31ca9eacd5782f09db5afcde1600ddc6b8b74548
ngocdai99 <ngocdai99@gmail.com>
2024-06-28 21:35:32 +0700

    add official brands to home

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -1,22 +1,20 @@
 package Activities;
-
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.example.stores.R;
 import com.example.stores.databinding.ActivityMainBinding;
-import com.google.android.material.navigation.NavigationBarView;
 
-import Adapters.MainViewPager2Adapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
 import Fragments.HomeFragment;
 import Fragments.MailFragment;
 import Fragments.NotificationFragment;
@@ -27,150 +25,53 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
-    MainViewPager2Adapter mainViewPager2Adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
         initUI();
+        setupBottomNav();
+    }
 
-        getWindow().setStatusBarColor(Color.parseColor("#F04D7F"));
-        getWindow().setNavigationBarColor(Color.WHITE);
-        getSupportActionBar().hide();
+    private void setupBottomNav(){
+        HashMap<Integer, Fragment> fragmentMap = new HashMap<>();
+        fragmentMap.put(R.id.homeMenu, new HomeFragment());
+        fragmentMap.put(R.id.categoryMenu, new MailFragment());
+        fragmentMap.put(R.id.receiptMenu, new VideoFragment());
+        fragmentMap.put(R.id.memberMenu, new NotificationFragment());
+        fragmentMap.put(R.id.profileMenu, new ProfileFragment());
 
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment fragment = fragmentMap.get(item.getItemId());
+            if (fragment != null) {
 
-        binding.toolbarTitleTv.setText("Home");
-//        setupViewPager();
+                String tag = fragment.getClass().getSimpleName();
 
-        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                Fragment fragment = null;
-                String tag = "";
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                if (itemId == R.id.homeMenu) {
-                    tag = "Home";
-                    fragment = fragmentManager.findFragmentByTag(tag);
-                    if (fragment == null) {
-                        fragment = new HomeFragment();
-                    }
-//                    binding.viewPager2.setCurrentItem(0);
-                    binding.toolbarTitleTv.setText("Home");
-                } else if (itemId == R.id.categoryMenu) {
-                    tag = "Mail";
-                    fragment = fragmentManager.findFragmentByTag(tag);
-                    if (fragment == null) {
-                        fragment = new MailFragment();
-                    }
-//                    binding.viewPager2.setCurrentItem(1);
-                    binding.toolbarTitleTv.setText("Mail");
-                } else if (itemId == R.id.receiptMenu) {
-                    tag = "Video";
-                    fragment = fragmentManager.findFragmentByTag(tag);
-                    if (fragment == null) {
-                        fragment = new VideoFragment();
-                    }
-//                    binding.viewPager2.setCurrentItem(2);
-                    binding.toolbarTitleTv.setText("Video");
-                } else if (itemId == R.id.memberMenu) {
-                    tag = "Notification";
-                    fragment = fragmentManager.findFragmentByTag(tag);
-                    if (fragment == null) {
-                        fragment = new NotificationFragment();
-                    }
-//                    binding.viewPager2.setCurrentItem(3);
-                    binding.toolbarTitleTv.setText("Notification");
-                } else if (itemId == R.id.profileMenu) {
-                    tag = "Profile";
-                    fragment = fragmentManager.findFragmentByTag(tag);
-                    if (fragment == null) {
-                        fragment = new ProfileFragment();
-                    }
-//                    binding.viewPager2.setCurrentItem(4);
-                    binding.toolbarTitleTv.setText("Profile");
-                }
-                fragmentTransaction
+                getSupportFragmentManager()
+                        .beginTransaction()
                         .replace(binding.frameLayout.getId(), fragment, tag)
                         .commit();
+
                 return true;
             }
+            return false;
         });
-
-
-//        binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                super.onPageSelected(position);
-//                binding.bottomNavigation.getMenu().getItem(position).setChecked(true);
-//            }
-//        });
-
-
     }
+
+
 
     private void initUI() {
+        getWindow().setStatusBarColor(Color.parseColor("#F04D7F"));
+        getWindow().setNavigationBarColor(Color.parseColor("#EFEFEF"));
+        Objects.requireNonNull(getSupportActionBar()).hide();
         binding.bottomNavigation.setSelectedItemId(R.id.homeMenu);
-        mainViewPager2Adapter = new MainViewPager2Adapter(this);
-//        binding.viewPager2.setAdapter(mainViewPager2Adapter);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(binding.frameLayout.getId(), new HomeFragment(), "HomeFragment")
+                .commit();
+
     }
-
-
-//        private void setupViewPager() {
-//            viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-//
-//            mViewPager.setAdapter(viewPagerAdapter);
-//
-//            mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//                @Override
-//                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//                }
-//
-//                @Override
-//                public void onPageSelected(int position) {
-//                    Fragment fragment = viewPagerAdapter.getItem(position);
-//                    backImv.setVisibility(View.GONE);
-//
-//
-//                    switch (position) {
-//                        case 0:
-//                            onSetPageSelected(R.id.homeMenu, "Home");
-//                            break;
-//                        case 1:
-//                            onSetPageSelected(R.id.categoryMenu, "Category");
-//                            break;
-//                        case 2:
-//                            onSetPageSelected(R.id.receiptMenu, "Receipt");
-//                            break;
-//                        case 3:
-//                            onSetPageSelected(R.id.memberMenu, "Member");
-//                            break;
-//                        case 4:
-//                            onSetPageSelected(R.id.profileMenu, "Profile");
-//                            break;
-//                        default:
-//                            onSetPageSelected(R.id.categoryMenu, "Category");
-//                            break;
-//                    }
-//                }
-//
-//                @Override
-//                public void onPageScrollStateChanged(int state) {
-//
-//                }
-//            });
-//        }
-
-//        private void onSetPageSelected (int iconName, String fragmentName) {
-//            bottomNavigation.getMenu().findItem(iconName).setChecked(true);
-//            toolbarTitleTv.setText(fragmentName);
-//            toolbar.setVisibility(View.VISIBLE);
-//        }
-
 }
 
--- app/src/main/java/Fragments/HomeFragment.java
+++ app/src/main/java/Fragments/HomeFragment.java
@@ -1,17 +1,25 @@
 package Fragments;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
+
 import com.example.stores.databinding.FragmentHomeBinding;
+
 import java.util.ArrayList;
+
+import Adapters.PopularBrandAdapter;
 import Adapters.SliderAdapter;
+import Models.OfficialBrand;
 import Models.SliderItem;
 
 public class HomeFragment extends Fragment {
@@ -26,6 +34,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(getLayoutInflater());
         initBanner();
+        initOfficialBrand();
         return binding.getRoot();
 
     }
@@ -42,7 +51,7 @@ public class HomeFragment extends Fragment {
         startSliderAutoCycle();
     }
 
-    private void startSliderAutoCycle(){
+    private void startSliderAutoCycle() {
         sliderHandler = new Handler();
         sliderRunnable = new Runnable() {
             @Override
@@ -53,15 +62,16 @@ public class HomeFragment extends Fragment {
                     nextItem = 0;
                 }
                 binding.viewPager2Slider.setCurrentItem(nextItem);
-                sliderHandler.postDelayed(this, 1500);
-
+                sliderHandler.postDelayed(this, 3000);
             }
         };
         sliderHandler.postDelayed(sliderRunnable, 1500);
     }
+
     private void stopSliderAutoCycle() {
         sliderHandler.removeCallbacks(sliderRunnable);
     }
+
     @Override
     public void onDestroyView() {
         stopSliderAutoCycle();
@@ -75,8 +85,27 @@ public class HomeFragment extends Fragment {
         compositePageTransformer.addTransformer(new MarginPageTransformer(40));
         binding.viewPager2Slider.setPageTransformer(compositePageTransformer);
         binding.indicator.setViewPager(binding.viewPager2Slider);
+    }
 
+    private void initOfficialBrand() {
+//        DatabaseReference popularRef = database.getReference("Items");
+//        binding.progressBarOfficialBrand.setVisibility(View.VISIBLE);
+        ArrayList<OfficialBrand> list = new ArrayList<>();
+
+        list.add(new OfficialBrand("Adidas", 1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1dtlrN5hP1x-m9AwA-NqGuUv2rwyehMoIkg&s"));
+        list.add(new OfficialBrand("Nike", 2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcWxm80LumKRT5BMbTqslFU9otu470BgW6LA&s"));
+        list.add(new OfficialBrand("Puma", 3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCP0XlN_ojXgAauWzIj9rk1XEGh-X-ixbieQ&s"));
+        list.add(new OfficialBrand("Zara", 4, "https://cdn.icon-icons.com/icons2/2845/PNG/512/zara_logo_icon_181327.png"));
+        list.add(new OfficialBrand("Chanel", 5, "https://i.pinimg.com/736x/d1/aa/c9/d1aac9ca68abe81dfbab955a9073167c.jpg"));
+
+        if (!list.isEmpty()) {
+            binding.recyclerViewOfficialBrand.setLayoutManager(new LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false));
+            binding.recyclerViewOfficialBrand.setAdapter(new PopularBrandAdapter(requireActivity(), list));
+        }
+        binding.progressBarOfficialBrand.setVisibility(View.GONE);
 
 
     }
+
+
 }
