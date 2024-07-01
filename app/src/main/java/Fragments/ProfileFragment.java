3b03889d920715096a04c6774a841fa58592c48a
ngocdai99 <ngocdai99@gmail.com>
2024-07-01 20:04:11 +0700

    create settings activity and handle log out

--- app/src/main/java/Fragments/ProfileFragment.java
+++ app/src/main/java/Fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import com.example.stores.databinding.FragmentHomeBinding;
 import com.example.stores.databinding.FragmentProfileBinding;
 
+import Activities.SettingsActivity;
+
 public class ProfileFragment extends Fragment {
     FragmentProfileBinding binding;
     @Nullable
@@ -20,6 +23,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(getLayoutInflater());
 
+        binding.imvSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireActivity(), SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
         return binding.getRoot();
     }
 }
